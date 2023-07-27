fe34758782816f570c258f426a609cf0d52afedc
Vladislav Titar <titar-vlad@mail.ru>
2023-07-27 21:24:20 +0300

    Stub and tests for findByName

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -19,4 +19,8 @@ public class PhoneBook {
         }
         return null;
     }
+
+    public String findByName(String name) {
+        return null;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -5,17 +5,27 @@ public class PhoneBookTest {
     @Test
     public void testAdd() {
         PhoneBook phoneBook = new PhoneBook();
-        int contactsCount = phoneBook.add("Vlad", "1234567890");
+        int contactsCount = phoneBook.add("Vladislav", "1234567890");
         assertEquals(1, contactsCount);
     }
 
     @Test
     public void testFindByNumber() {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("Vlad", "1234567890");
-        phoneBook.add("Dima", "9876543210");
+        phoneBook.add("Vladislav", "1234567890");
+        phoneBook.add("Polina", "9876543210");
 
         String name = phoneBook.findByNumber("1234567890");
-        assertEquals("Vlad", name);
+        assertEquals("Vladislav", name);
+    }
+
+    @Test
+    public void testFindByName() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Vladislav", "1234567890");
+        phoneBook.add("Polina", "9876543210");
+
+        String number = phoneBook.findByName("Vladislav");
+        assertEquals("1234567890", number);
     }
 }
